5884415d3b21881c98ecb6d6d571bc12d051927c
Eva Ribes <eva.ribes@etu.univ-amu.fr>
2024-11-06 09:34:06 +0100

    ajout methode getitem et str

--- ribes_eva_tp5.py
+++ ribes_eva_tp5.py
@@ -1,10 +1,45 @@
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        p = Point(self.x, self.y)
+    def __init__(self, *args):
+        if len(args) == 0:
+            self.x = 0
+            self.y = 0
+        elif len(args) == 1:
+            self.x = args[0].x
+            self.y = args[0].y
+        elif len(args) == 2:
+            self.x = args[0]
+            self.y = args[1]
+
+
+        
+
     
+    def __getitem__(self, i):
+        if i == 0:
+           return self.x
+        elif i == 1:
+            return self.y
+        else:
+            return IndexError
+        
+    
+
+    def __str__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) +')'
+
+
+
+
+
+
+ 
 p = Point(1,2)
+Point(p)
+
+#print(p.__getitem__(10))  #<class 'IndexError'>
+#print(p.__getitem__(1))   #2
+
+print(p.__str__())
 
 
 

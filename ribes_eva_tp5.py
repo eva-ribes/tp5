da52f1596633abb6d2b0ef687a3719fb915119e8
Eva Ribes <eva.ribes@etu.univ-amu.fr>
2024-11-13 08:17:47 +0100

    ajout de la méthode determinant

--- ribes_eva_tp5.py
+++ ribes_eva_tp5.py
@@ -47,6 +47,13 @@ class Point:
 
     def distance_point(self, p):
         return sqrt((self.x - p.x)**2 + (self.y - p.y)**2)
+    
+    
+    def determinant(self, p, d):
+        ls_1 = [p.x - self.x, p.y -self.y]
+        ls_2 = [d.x - self.x, d.y -self.y]
+        return ls_1[0]*ls_2[1] - ls_1[1]*ls_2[0]
+
 
 
  
@@ -54,6 +61,8 @@ p = Point(1, 2)
 Point(p)
 
 d = Point(2, 3)
+
+b = Point(4, 4)
 
 #print(p.__getitem__(10))  #<class 'IndexError'>
 #print(p.__getitem__(1))   #2
@@ -69,5 +78,13 @@ d = Point(2, 3)
 
 #print(p.distance_point(d))  #1.4142135623730951
 #print(p.distance_point(p))  #0.0
+
+#print(p.determinant(d, b))  #-1
+#print(d.determinant(p, b))  #1
+
+
+
+
+
 
 
